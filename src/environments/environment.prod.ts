e55e0684f326e18d7ed24faf861cd1eee9d176ce
lucasguarasp <lucasaguiarpaixao@gmail.com>
2024-11-07 12:57:04 -0300

    remove enviroment

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://wflow-bot-api-getway.vercel.app/api/gateway/',
-  apiUrlAuth: 'https://jwt-auth-node.vercel.app/'
-};
--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-
-
-
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api/gateway/',
-  apiUrlAuth: 'http://localhost:4000/'
-};
