c7365b3dc87ee3e9d42c653b6049537e67bb340f
lucasguarasp <lucasaguiarpaixao@gmail.com>
2024-11-25 16:20:11 -0300

    remove enviroment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wflow-bot'`, () => {
+  it(`should have as title 'compare-flow'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('wflow-bot');
+    expect(app.title).toEqual('compare-flow');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, wflow-bot');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, compare-flow');
   });
 });
